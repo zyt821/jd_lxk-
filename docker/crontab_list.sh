106c02076349f0455c584a3b395b5dd657256a3d
LingFeng <lingfeng_0918@163.com>
2021-07-12 23:57:43 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -136,7 +136,7 @@
 # 京东秒秒币
 10 7 * * * node /scripts/jd_ms.js >> /scripts/logs/jd_ms.log 2>&1
 # 京喜财富岛
-10 * * * *  node /scripts/jd_cfd2.js >> /scripts/logs/jd_cfd2.log 2>&1
+0 * * * *  node /scripts/jd_cfd.js >> /scripts/logs/jd_cfd.log 2>&1
 # 京喜财富岛提现
 0 0 * * *  node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.log 2>&1
 # 京东价格保护
