dc9748c2043a106bd752ca2a5b4409b5a571530c
LingFeng <lingfeng_0918@163.com>
2021-07-16 11:13:38 +0800

    懒得改了，直接搬运大佬的

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -177,6 +177,8 @@
 20 0,20 * * * node /scripts/jd_necklace3.js >> /scripts/logs/jd_necklace3.log 2>&1
 #汪汪乐园
 30 2,20 * * * node /scripts/jd_joy_park.js >> /scripts/logs/jd_joy_park.log 2>&1
+#省钱大赢家之翻翻乐
+20 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 ##############默认注释活动##############
 # 京东试用（默认注释，请配合取关脚本使用）
 #10 0 * * *  node /scripts/jd_try.js >> /scripts/logs/jd_try.log 2>&1
