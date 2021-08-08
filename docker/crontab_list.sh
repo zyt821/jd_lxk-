8dc5641456e6f8f38563ff2337f201ba81217afa
LingFeng <lingfeng_0918@163.com>
2021-08-08 13:04:24 +0800

    更新新一期女装魔盒

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,7 +7,7 @@
 30 9 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 # 超级直播间红包雨
 0,30,31 0-23/1 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
-# 女装盲盒 活动时间：2021-05-1到2021-05-31
+# 女装盲盒 活动时间：2021-8-5至2021-8-31
 35 1,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 # 京东国际盲盒活动时间】2021年02月23日 起至 2021年03月31日 18:00:00
 5 7,12,23 * * * node /scripts/jd_global_mh.js >> /scripts/logs/jd_global_mh.log 2>&1
