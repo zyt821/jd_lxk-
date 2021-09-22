1a2d39003ca03a901fd3ed75cd9829efd4674bd2
LingFeng <lingfeng_0918@163.com>
2021-09-22 18:34:59 +0800

    add-东东游戏

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -91,6 +91,8 @@
 20 5 * * * node /scripts/jd_ddwj.js >> /scripts/logs/jd_ddwj.log 2>&1
 #热血心跳,狂解压
 15 6,18 1-16,21-30 9,10 * node /scripts/jd_decompression.js >> /scripts/logs/jd_decompression.log 2>&1
+#东东游戏
+35 12-23/4 * * * node /scripts/jd_ddgame.js >> /scripts/logs/jd_ddgame.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
